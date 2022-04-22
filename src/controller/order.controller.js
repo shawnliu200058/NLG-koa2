3b480ffa0357b6dd662f13361d2aa80531a2b38d
shelwinliu <1455483468@qq.com>
2022-04-22 22:19:27 +0800

    ‘增加支付宝沙箱，但仍需完善’

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -1,10 +1,46 @@
+const AlipayFormData = require('alipay-sdk/lib/form').default
+const snowId = require('simple-flakeid')
+
+const alipaySdk = require('../utils/alipayKeyTool/alipay')
 const service = require('../service/order.service')
 
 class OrderController {
   async create(ctx) {
     // console.log(ctx.request.body)
-    const result = await service.create(ctx.request.body)
-    ctx.body = result
+    let gen1 = new snowId.SnowflakeIdv1({ workerId: 1 })
+    // 生成订单编号
+    const orderID = gen1.NextId()
+    // console.log(orderID)
+    const formData = new AlipayFormData()
+    // console.log(formData)
+    formData.setMethod('get')
+    formData.addField(
+      'returnUrl',
+      'http://localhost:8080/#/pages/paymentSucceeded/paymentSucceeded'
+    )
+    formData.addField('bizContent', {
+      outTradeNo: orderID,
+      productCode: 'FAST_INSTANT_TRADE_PAY',
+      totalAmount: '0.01',
+      subject: orderID,
+      body: '商品详情'
+    })
+    // console.log(formData)
+    const result = await alipaySdk.exec(
+      'alipay.trade.wap.pay',
+      {},
+      { formData }
+    )
+    // console.log(result)
+    // result.then((resp) => {
+    //   console.log(resp)
+    //   ctx.body = { resp }
+    // })
+    // console.log(result)
+    ctx.body = { url: result }
+
+    // const result = await service.create(ctx.request.body)
+    // ctx.body = result
   }
 
   async getOrderInfo(ctx) {
---
+++ src/utils/alipayKeyTool/alipay.js
@@ -0,0 +1,27 @@
+const AlipaySdk = require('alipay-sdk').default
+const fs = require('fs')
+const path = require('path')
+
+const alipaySdk = new AlipaySdk({
+  appId: '2021000119675383',
+  signType: 'RSA2',
+  gateway: 'https://openapi.alipaydev.com/gateway.do',
+  alipayPublicKey: fs.readFileSync(
+    path.resolve(__dirname, './keys/publicKey.pem'),
+    'utf-8'
+  ),
+  privateKey: fs.readFileSync(
+    path.resolve(__dirname, './keys/privateKey.pem'),
+    'utf-8'
+  )
+})
+
+// const alipaySdk = () => {
+//   console.log(
+//     fs.readFileSync(path.resolve(__dirname, './keys/publicKey.pem'), 'utf-8')
+//   )
+// }
+
+// alipaySdk()
+
+module.exports = alipaySdk
